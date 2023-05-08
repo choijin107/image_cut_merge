29d3675affea0d4e82db25d7202bc38f9b8d11d8
choijin107 <45784739+choijin107@users.noreply.github.com>
2023-05-09 08:02:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import merge_image_template
 
 if __name__ == '__main__':
 
-    #cut_image.main("lena.png", 5, 4, "lena_cut")
+    cut_image.main("lena.png", 5, 4, "lena_cut")
     merge_image.main("lena_cut", 5, 4, "lena_merge.png")
     #merge_image_template.main("lena_cut", 5, 4, "lena_merge.png")
